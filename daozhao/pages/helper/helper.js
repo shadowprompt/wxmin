fc769e7858d174b3b96559fa1a869bbdfe2d4257
sp <huixiong.cn@gmail.com>
2018-06-12 23:05:56 +0800

    'ewe'

--- daozhao/pages/helper/helper.js
+++ daozhao/pages/helper/helper.js
@@ -3,17 +3,27 @@ Page({
   data: {
     searchPlaceHolder:"请输入您的上海公交卡号",
     searchLabel:"查询",
-    balance:""
+    balance:"",
+    cardNo:null,
   },
   myBack: function(data){
     console.log("data= " + data);
   },
+  cardNoInput: function(e){
+    this.setData({
+      cardNo: e.detail.value
+    });
+  },
   //事件处理函数
   sptccSearch: function() {
+    var that = this;
+    that.setData({
+      balance: '余额查询中...'
+    });
     wx.request({
-      url: 'https://www.daozhao.com/w/wxmin/tools/sptcc.html?arg1=11586532332&cb=myBack', //上海公交卡余额查询接口地址
+      url: 'https://www.daozhao.com.cn/get', //上海公交卡余额查询接口地址
       data: {
-        "arg1":"66386386229"
+        "cardNo": that.data.cardNo
       },
       method:"get",
       dataType: "jsonp",
@@ -21,7 +31,22 @@ Page({
           'content-type': 'application/json'
       },
       success: function(res) {
-        console.log(res)
+        console.log(res);
+        if(res.statusCode === 200){
+          var money = /transportMoney\('(\d*\.*\d*)'\);/.test(res.data);
+          var balance = RegExp.$1;
+          console.log({
+            money: RegExp.$1
+          });
+          that.setData({
+            balance: `余额：${balance}`
+          });
+        }else{
+          that.setData({
+            balance: '查询失败'
+          });
+        }
+        
       }
     })
   }
